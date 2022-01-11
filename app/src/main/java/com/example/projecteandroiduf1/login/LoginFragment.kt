81aa1cac9e0c178102ecabf0cf0c246d105b51fb
hugos <asihkas@ashoij.com>
2022-01-11 15:51:22 +0100

    opo

--- app/src/main/java/com/example/projecteandroiduf1/login/LoginFragment.kt
+++ app/src/main/java/com/example/projecteandroiduf1/login/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.example.projecteandroiduf1.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,9 +9,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import com.example.androiduf1.database.Userdatabase
-
 import com.example.projecteandroiduf1.R
 import com.example.projecteandroiduf1.databinding.FragmentLoginBinding
 
@@ -23,20 +22,24 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
+            R.layout.fragment_login,container,false)
+
         val application = requireNotNull(this.activity).application
         val database = Userdatabase.getInstance(application).LoginDatabaseDao
         val viewModelFactory = LoginViewModelFactory(database, application)
 
         val viewModel: LoginViewModel = ViewModelProvider(this,viewModelFactory).get(LoginViewModel::class.java)
 
-        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
-            R.layout.fragment_login,container,false)
+
 
         binding.Loginbutton.setOnClickListener { view : View ->
             view.findNavController().navigate(R.id.action_LoginFragment_to_mainFragment)
+            Log.i("Loginbutton", "Loginbutton press")
+
         }
 
-        //binding.loginViewModel = viewModel
+        binding.loginViewModel = viewModel
         binding.setLifecycleOwner(this)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
